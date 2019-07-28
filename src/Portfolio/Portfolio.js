b9a4aff679fafc21298895c6d4509f841c18c52b
igor popenov <ipopenov@protonmail.com>
2019-07-28 18:49:31 -0400

    Added some chart components that are semi functional

---
+++ src/Portfolio/Portfolio.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import SearchAppBar from './SearchAppBar';
+import LabelBottomNavigation from './LabelBottomNavigation';
+import Box from '@material-ui/core/Box';
+import Row from './Row';
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Donut from './Charts/Donut';
+import InteractivePieChart from './Charts/interactive-pie';
+import WithSize from './Charts/with-size';
+import LineChart from './Charts/LineChart';
+import LineGraph from './Charts/LineGraph';
 
 const MyApp = () => {
     return (
         <>
-            <CssBaseline>
-                <App />
-            </CssBaseline>
+
+            <LineChart />
+            <LineGraph />
         </>
     )
 }
